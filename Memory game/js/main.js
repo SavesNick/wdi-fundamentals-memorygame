764e25a5a3dd592a1678bc4f4aa31a033ea5f1ef
Nikita Savelev <48187086+SavesNick@users.noreply.github.com>
2020-04-22 10:47:06 -0400

    Added game logic to memory game using if statements

--- Memory game/js/main.js
+++ Memory game/js/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running!");
-let cardOne = "queen"
-let cardTwo = "king"
-let cardThree = "queen"
-let cardFour = "king"
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+//console.log("User flipped " + cardThree);
+alert('Hello, friends.');
+
+//cards array
+let cards = ["queen", "king", "queen", "king"]
+let cardsInPlay = [ ]
+let cardOne = cards[0]
+let cardTwo = cards[1]
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+if(cardsInPlay.length===2){
+if(cardsInPlay[0]===cardsInPlay[1]){alert("You found a match!");}else{alert("Sorry, try again.");}
+}
