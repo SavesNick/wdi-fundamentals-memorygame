d3bd0b775dbddd9eac0e09aca4f18dc306fa4ae8
Nikita Savelev <48187086+SavesNick@users.noreply.github.com>
2020-04-23 13:03:54 -0400

    Changed cards from an array into an object
    and changed the flip card function to add keys to the empty array

--- Memory game/js/main.js
+++ Memory game/js/main.js
@@ -3,21 +3,41 @@ console.log("Up and running!");
 alert("Hello, friends.");
 
 //cards array
-let cards = ["queen", "king", "queen", "king"];
+let cards = [{
+rank:"queen",
+suit:"hearts",
+cardImg:"images/queen-of-hearts.png"
+},
+{
+  rank:"queen",
+  suit:"diamonds",
+  cardImg:"images/queen-of-diamonds.png"
+  },{
+    rank:"king",
+    suit:"hearts",
+    cardImg:"images/king-of-hearts.png"
+    },{
+      rank:"king",
+      suit:"diamonds",
+      cardImg:"images/king-of-diamonds.png"
+      }
+];
 //empty array
 let cardsInPlay = [];
 function flipCard(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].suit)
+  console.log(cards[cardId].cardImg)
 }
 flipCard(0);
-flipCard(1);
+flipCard(2);
 function checkForMatch() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
     } else {
-      console.log("Sorry, try again.");
+      alert("Sorry, try again.");
     }
   }
 }
